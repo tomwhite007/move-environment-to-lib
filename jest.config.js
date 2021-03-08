fafc0f48275e983e14832db17da54a0b0f23d33c
Tom White <tomwhite007@gmail.com>
2021-03-08 07:55:25 +0000

    scaffold data-access lib

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
     '<rootDir>/libs/shared/ui-my-configurable-component',
     '<rootDir>/libs/injectors-demo/feature-shell',
     '<rootDir>/libs/injectors-demo/util-environment',
+    '<rootDir>/libs/shared/data-access-config',
   ],
 };
